1180857473eeb79f06aa7fdd1927ec3c81511997
Phatrycio Miranda <phatrycio137@gmail.com>
2025-11-03 16:48:15 -0300

    TODOS e Reminder

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,35 @@
 "use strict";
-alert("ta gravando?");
+(function () {
+    var todo = {
+        description: 'todo',
+        done: false,
+    };
+    var reminder = {
+        description: 'reminder',
+        date: '07.11.2025',
+    };
+    var taskView = {
+        render: function (tasks) {
+            var tasksList = document.getElementById('tasksList');
+            while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement('LI');
+                var textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
+            });
+        }
+    };
+    var TaskController = function (view) {
+        var _a;
+        var tasks = [todo, reminder];
+        var handleEvent = function (event) {
+            event.preventDefault();
+            view.render(tasks);
+        };
+        (_a = document.getElementById('taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
+    };
+    TaskController(taskView);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-alert("ta gravando?")
+(() => {
+    const todo = {
+        description: 'todo',
+        done: false,
+    }
+
+    const reminder = {
+        description: 'reminder',
+        date: '07.11.2025',
+    }
+
+    const taskView = {
+        render(tasks: Array<Object>) {
+            const tasksList = document.getElementById('tasksList');
+            while (tasksList?.firstChild) {
+                tasksList.removeChild(tasksList.firstChild)
+            }
+
+            tasks.forEach((task) => {
+            const li = document.createElement('LI')
+            const textNode = document.createTextNode(JSON.stringify(task))
+            li.appendChild(textNode)
+            tasksList?.appendChild(li) 
+            })
+        }
+    }
+
+    const TaskController = (view: typeof taskView) => {
+        const tasks: Array<Object> = [todo, reminder]
+
+        const handleEvent = (event: Event) => {
+            event.preventDefault()
+            view.render(tasks)
+        }
+
+        document.getElementById('taskForm')?.addEventListener('submit', handleEvent)
+    }
+
+    TaskController(taskView)
+}) ()
